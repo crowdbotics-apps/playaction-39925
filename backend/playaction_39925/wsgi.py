63345727d21a20708024489de8520728e564b6e5
Crowdbotics <team@crowdbotics.com>
2023-04-14 07:06:24 +0000

    Initial commit.

---
+++ backend/playaction_39925/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for playaction_39925 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'playaction_39925.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "playaction-39925",
+  url: "https://playaction-39925.botics.co",
+  api: "https://playaction-39925.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
